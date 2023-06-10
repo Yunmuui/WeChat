62f961bac49650649c0e9863051521e3c785a777
Yunmuui <qwerqwer791346@163.com>
2023-06-10 20:49:40 +0800

    ChatsAdapter.java 为每个聊天列表的每一项添加点击事件，点击后intent跳转聊天窗口并传参（聊天所属用户、聊天标题、本地聊天记录内容、本地聊天记录在数据库中的id）；构造方法增加context参数，因为使用intent需要context，而在adapter内部难以取到context Chat.java 为了能被intent传参，其内部类OneChat实现了Parcelable接口，实现了相应的方法 ChatsFragment.java 用了改完后的ChatsAdapter的构造方法，将fragment的context传入 wechat_chats_item.xml 将聊天列表的每一项的高度从match_parent改为wrap_content，以避免一项占一面。实现正确的显示效果 ChatActivity.java 接受传参，并完成聊天标题的设置，还有聊天记录没有设置。

--- app/src/main/java/com/jsut/wechat/activity/ChatActivity.java
+++ app/src/main/java/com/jsut/wechat/activity/ChatActivity.java
@@ -1,16 +1,38 @@
 package com.jsut.wechat.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.jsut.wechat.Entity.Chat;
 import com.jsut.wechat.R;
 
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
+    TextView chat_name;
+    String user;
+    RecyclerView msg_recycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // 获取传递的参数
+        user = getIntent().getStringExtra("user");
+        String chatTitle = getIntent().getStringExtra("chatTitle");
+        List<Chat.OneChat> chatContent = getIntent().getParcelableArrayListExtra("chatContent");
+        int id = getIntent().getIntExtra("id", 0);
+
+        //获取控件
+        chat_name=findViewById(R.id.chat_name);
+        msg_recycle=findViewById(R.id.msg_recycle);
+
+        //设置控件
+        chat_name.setText(chatTitle);
+
     }
 }
