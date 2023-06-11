1d0f0d15e7e342fa0dca0d267e465cab23139855
Yunmuui <qwerqwer791346@163.com>
2023-06-11 22:01:56 +0800

    activity_chat.xml 临时加了一个imageview来显示拍摄的图片 ChatActivity.java 实现拍摄，图片放imageview并编码成base64的功能 AndroidManifest.xml 拍照权限、相机和仿微信的provider file_paths.xml 相机和仿微信的provider的共享文件目录

--- app/src/main/java/com/jsut/wechat/activity/ChatActivity.java
+++ app/src/main/java/com/jsut/wechat/activity/ChatActivity.java
@@ -2,17 +2,33 @@ package com.jsut.wechat.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.utils.widget.ImageFilterButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jsut.wechat.Dao.ChatsDao;
 import com.jsut.wechat.DataBase.ChatsDatabase;
@@ -22,9 +38,14 @@ import com.jsut.wechat.R;
 import com.jsut.wechat.adapter.MsgAdapter;
 import com.jsut.wechat.fragment.ChatsFragment;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+    public static final int TAKE_PHOTO = 101;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE=102;
     TextView chat_name;
     String user;
     RecyclerView msg_recycle;
@@ -33,9 +54,13 @@ public class ChatActivity extends AppCompatActivity {
     EditText input;
 
     Button back;
+    ImageFilterButton shoot_button;
+    ImageView shoot_photo;
+    private Uri imageUri;
 
     LinearLayout more_menu;
     LinearLayout chat_window;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,10 +81,13 @@ public class ChatActivity extends AppCompatActivity {
         chat_window=findViewById(R.id.chat_window);
         input = (EditText)findViewById(R.id.input_text);
         Button send = (Button) findViewById(R.id.send);
+        shoot_button = findViewById(R.id.shoot_button);
+        shoot_photo = findViewById(R.id.shoot_photo);
 
         //设置控件
         chat_name.setText(chatTitle);
         setOnTouchToCloseMoreMenu();
+        setOnClickToShoot();
 
         //信息显示
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -90,6 +118,125 @@ public class ChatActivity extends AppCompatActivity {
                 input.setText("");
             }
         });
+    }
+
+    /**
+     * 获取活动或片段的位图和图像路径onActivityResult
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (bitmap != null) {
+                shoot_photo.setImageBitmap(bitmap); // 展示刚拍过的照片
+                getImgBase64(shoot_photo); // 直接把 imageview 取出图片转换为base64格式
+            } else {
+                Toast.makeText(this.getApplicationContext(), "Picture not taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+    /**
+     * imageview取出图片转换为base64格式
+     *
+     * @param imageView
+     * @return
+     */
+    public String getImgBase64(ImageView imageView) {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+        byte[] bb = bos.toByteArray();
+        String image = Base64.encodeToString(bb, Base64.NO_WRAP);
+        return image;
+    }
+    /**
+     * 将字符串转换成Bitmap类型
+     *
+     * @param string
+     * @return
+     */
+    public static Bitmap stringtoBitmap(String string,ImageView imageView) {
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            bitmapArray = Base64.decode(string, Base64.DEFAULT);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
+
+            imageView.setImageBitmap(bitmap); // 展示图片
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    private void setOnClickToShoot() {
+        shoot_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(ChatActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    //无相机权限
+                    ActivityCompat.requestPermissions(ChatActivity.this, new String[] { Manifest.permission.CAMERA }, CAMERA_PERMISSION_REQUEST_CODE);
+                }else{
+                    //有相机权限
+                    startCamera();
+                }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 相机权限已经被授予
+                startCamera();
+            } else {
+                // 相机权限被拒绝
+                Toast.makeText(this, "相机权限被拒绝", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startCamera() {
+        //创建file对象，用于存储拍照后的图片；
+        File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(ChatActivity.this,
+                    "com.jsut.wechat.fileprovider", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        //启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
     }
 
     private void setOnTouchToCloseMoreMenu() {
